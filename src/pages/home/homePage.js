11e725b10ef7caa25c1a04973cb0a43f36f09889
maximetouroute <maxime.touroute@gmail.com>
2018-11-12 11:00:58 +0100

    githubpages homepage propety & trick for fullscreen activation

--- src/pages/home/homePage.js
+++ src/pages/home/homePage.js
@@ -19,12 +19,26 @@ export class HomePage extends Component {
             isFull: false,
         };
     }
+
+    openFullscreen() {
+        const elem = document.documentElement;
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.mozRequestFullScreen) { /* Firefox */
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE/Edge */
+            elem.msRequestFullscreen();
+        }
+    }
+
     render() {
         return (
             <div className="homePage">
                 <div className="mainGrid">
                     <div className="message">
-                            <img src={homepageContent.logo} width={400} style={{margin:`auto`}}/>
+                            <img src={homepageContent.logo} width={400} style={{margin:`auto`}} onClick={() => {this.openFullscreen()}}/>
 
                         <div className="text">
                             <div className="small">
